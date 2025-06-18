951c41de1730606f6f512601921c91f3c6a96577
praveeng <erpraveenganapathy@gmail.com>
2025-06-18 10:16:41 -0400

    Lab 2

---
+++ samosa-selector/src/App.jsx
@@ -0,0 +1,66 @@
+import './App.css';
+import { useState } from 'react';
+
+
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier)
+
+  const buyDoubleStuffed = () => {
+    if (count >=10){
+      setMultiplier(multiplier*2)
+      setCount(count-10)
+    }
+  }
+
+    const buyPartyPack = () => {
+    if (count >=100){
+      setMultiplier(multiplier*5)
+      setCount(count-100)
+    }
+  }
+
+    const buyFullFeast = () => {
+    if (count >=1000){
+      setMultiplier(multiplier*10)
+      setCount(count-1000)
+    }
+  }
+  return (
+    <div className='App'>
+      <div className='header'>
+        <h1> Samosa Selector</h1>
+        <h2> Count: {count}</h2>
+        <img className="samosa" src="https://png.pngtree.com/png-vector/20240804/ourmid/pngtree-mouth-watering-chicken-samosas-with-a-crispy-golden-crust-png-image_13373256.png" onClick={updateCount} alt="" />
+      </div>
+      <div className='container'>
+
+        <div className='upgrade'>
+          <h3>Double Stuffed 👯‍♀️	</h3>
+          <p>2x per click	</p>
+          <button onClick={buyDoubleStuffed}>10 samosas</button>
+        </div>
+
+        <div className='upgrade'>
+          <h3>Party Pack 🎉	</h3>
+          <p>5x per click	</p>
+          <button onClick={buyPartyPack}>100 samosas</button>
+        </div>
+
+        <div className='upgrade'>
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 samosas</button>
+        </div>
+
+      </div>
+
+    </div>
+  )
+
+}
+export default App
